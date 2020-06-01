42da60f089e3b9d47318ec5c83ffbbf6474a6c0e
rahul <shahrahul795@gmail.com>
2020-06-02 01:38:24 +0530

    first project of age

---
+++ static/index.js
@@ -0,0 +1,13 @@
+// challenge 1: your age in days
+function ageInDays() {
+  var birthYear = prompt("what year you were born in");
+  var ageInDayss = (2020 - birthYear) * 365;
+  var h1 = document.createElement("h1");
+  var TextAnswer = document.createTextNode("youre" + ageInDayss + "days old");
+  h1.setAttribute("id", "ageInDays");
+  h1.appendChild(TextAnswer);
+  document.getElementById("flex-box-result").appendChild(h1);
+}
+function reset() {
+  document.getElementById("ageInDays").remove();
+}
